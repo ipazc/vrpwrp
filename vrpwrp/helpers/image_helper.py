60127132155f06a21e9267af933109ddd1c5fff0
Iván <ipazce00@estudiantes.unileon.es>
2017-09-20 16:40:25 +0200

    Fixed two mistakes: 1) get_image() not returning PIL image; and 2) to_byte_array() not closing the IOBytes object. Added documentation for the headers of the helper's functions.

--- vrpwrp/helpers/image_helper.py
+++ vrpwrp/helpers/image_helper.py
@@ -12,29 +12,56 @@ HORIZONTAL = 0
 VERTICAL = 1
 
 def get_file_binary_content(uri):
+    """
+    Retrieves the binary content of a file URI.
+    :param uri: path to the file to be loaded
+    :return: raw bytes list of the file content.
+    """
     with open(uri, "rb") as f:
         c = f.read()
 
     return c
 
-def get_file_image(URI):
-    with Image.open(URI) as im:
+def get_file_image(uri):
+    """
+    Retrieves the file as a PIL image.
+    :param uri: path to the file to be loaded
+    :return: PIL image representing the loaded image.
+    """
+    with Image.open(uri) as im:
         image = im.convert("RGB")
     return image
 
 def get_image(image_bytes):
+    """
+    Retrieves the PIL image from the given array of bytes.
+    :param image_bytes: bytes to convert into a PIL image.
+    :return: PIL image.
+    """
     with Image.open(io.BytesIO(image_bytes)) as im:
         image = im.convert("RGB")
-    return
+    return image
 
 def to_byte_array(pil_image):
-    bytes_io = io.BytesIO()
-    pil_image.save(bytes_io, "PNG")
-    bytes_io.seek(0)
-    return bytes_io
+    """
+    converts the PIL image into a bytes array.
+    :param pil_image: PIL image to convert to
+    :return: Bytes array representing the image.
+    """
+    with io.BytesIO() as bytes_io:
+        pil_image.save(bytes_io, "PNG")
+        bytes_io.seek(0)
+        result = bytes_io.read()
+
+    return result
 
 def crop_by_bbox(pil_image, bbox):
-
+    """
+    Crops the specified PIL image with the given bounding box
+    :param pil_image: PIL image to crop
+    :param bbox: bounding box object to crop by
+    :return: PIL image cropped.
+    """
     box = bbox.get_box()
     box[2] += box[0]
     box[3] += box[1]
@@ -43,16 +70,32 @@ def crop_by_bbox(pil_image, bbox):
 
     return crop_result
 
-def get_cropped_faces(image_bytes, bounding_boxes):
-    return [crop_by_bbox(image_bytes, bounding_box) for bounding_box in bounding_boxes]
+def get_cropped_faces(pil_image, bounding_boxes):
+    """
+    Crops the specified PIL image by the specified bounding boxes
+    :param pil_image: PIL image to crop by
+    :param bounding_boxes: bounding boxes list to crop image.
+    :return: A list of cropped images.
+    """
+    return [crop_by_bbox(pil_image, bounding_box) for bounding_box in bounding_boxes]
 
-def segment_image(pil_image, segment_width, segment_height, iteration=HORIZONTAL):
+def segment_image(pil_image, segment_width, segment_height, iteration_order=HORIZONTAL):
+    """
+    Generator of segments of a PIL image. This generator breaks the PIL image into a
+    subset of PIL images. Like in a Puzzle, it returns one by one on each iteration_order. The iteration_order order can be
+     HORIZONTAL (going from TOP-LEFT to TOP-RIGHT) or VERTICAL (going from TOP-LEFT to BOTTOM-LEFT).
+    :param pil_image: Image to segment.
+    :param segment_width: the width of the fragment.
+    :param segment_height: the height of the fragment.
+    :param iteration_order: order of the iteration_order.
+    :return:
+    """
     width, height = pil_image.size
 
     horizontal_segment_count = width // segment_width + int(width % segment_width > 0)
     vertical_segment_count = height // segment_height + int(height % segment_height > 0)
 
-    if iteration==VERTICAL:
+    if iteration_order==VERTICAL:
         for x in range(horizontal_segment_count):
             for y in range(vertical_segment_count):
                 bounding_box = BoundingBox(x * segment_width, y * segment_height,
